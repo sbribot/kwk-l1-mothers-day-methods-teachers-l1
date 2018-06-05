c60e7fe716f3608de117334e7cfc889e0114ed00
Sara Beth Ribot <sbribot@gmail.com>
2018-06-05 01:09:44 +0000

    Done.

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,6 +1,22 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
-
+def mothers_day
+  puts "Happy Mother's Day, Mom!"
+end 
 
 ## Call your method below - use puts to see it in the terminal!
+
+puts mothers_day
+
+def personalized_mothers_day(name)
+  puts "Happy Mother's Day, #{name}!"
+end
+
+puts personalized_mothers_day
+
+def new_mothers_day(name = "Mom")
+  puts "Happy Mother's Day, #{name}!"
+end
+
+puts new_mothers_day
 
