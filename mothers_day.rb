df53990f4e9746742fbd2e53b93176fe83d0c7f5
Sara Beth Ribot <sbribot@gmail.com>
2018-06-05 01:00:42 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,6 +1,8 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
-
+def mothers_day
+  puts "Happy Mother's Day, Mom!"
 
 ## Call your method below - use puts to see it in the terminal!
 
+puts mothers_day
